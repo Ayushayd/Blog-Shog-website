a802a73e192db422ae0e62ac02b4099d271e18f1
Ayush <127881117+Ayushayd@users.noreply.github.com>
2024-05-02 19:28:41 +0530

    changes in dbConnection.js file

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = ()=> {
     mongoose.connect(process.env.MONGO_URI,{
         dbName: "CHIT_CHAT"
-    },{useNewUrlParser: true, useUnifiedTopology: true}).then(() =>{
+    }).then(() =>{
         console.log("Connected to database");
     }).catch(err=>{
         console.log(`Some error occured while connecting to database: ${err}`);
