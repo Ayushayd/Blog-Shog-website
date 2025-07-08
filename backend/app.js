d165c94f6c36f03c71eb92ae3bc5f6974782eb4c
Ayush Gupta <ayushe19222@gmail.com>
2025-07-09 00:44:32 +0530

    added cron job scheduler

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,50 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import {dbConnection} from './database/dbConnection.js'
-import {errorMiddleware} from "./middleware/error.js"
-import userRouter from './routes/userRouter.js';
-import blogRouter from './routes/blogRouter.js'
-import fileUpload from 'express-fileupload';
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middleware/error.js";
+import userRouter from "./routes/userRouter.js";
+import blogRouter from "./routes/blogRouter.js";
+import fileUpload from "express-fileupload";
+import job from "./config/cron.js";
 
 const app = express();
 dotenv.config({
-    path: "./config/.env"
+  path: "./config/.env",
 });
 
-app.use(cors({
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
+
+app.use(
+  cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "PUT", "DELETE", "POST"],
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(fileUpload({
+app.use(
+  fileUpload({
     useTempFiles: true,
-    tempFileDir: "/tmp/"
-}));
-
+    tempFileDir: "/tmp/",
+  })
+);
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/blog", blogRouter);
 
 dbConnection();
-app.get('/', (req, res) => {
-    res.send("Express on vercel");
-})
+app.get("/", (req, res) => {
+  res.send("Express on vercel");
+});
 
 app.use(errorMiddleware);
-
 
 export default app;
